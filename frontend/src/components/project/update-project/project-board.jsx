0664e7ebc98a4e9ebac53aaa0ebd6d70714d5aa0
Balázs Miklós <balazs.miklos@yahoo.com>
2022-12-09 17:38:48 +0100

    Projekt táblázat létrehozás továbbfejlesztése

--- frontend/src/components/project/update-project/project-board.jsx
+++ frontend/src/components/project/update-project/project-board.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { processesByProject } from "../../../api/project/process.queries";
 import { ProcessBoardCard } from "./project-board/process-board-card/process-board-card";
+import { ProjectBoardProcessCreate } from "./project-board/project-board-process-create/project-board-process-create";
 import "./project-board/project-board.css";
 
 export function ProjectBoard({ project }) {
@@ -20,6 +21,11 @@ export function ProjectBoard({ project }) {
     return <div className="aae-project-board">
 
         <div className="aae-project-board__processes">
+
+            <div className="aae-project-board__process-create">
+                <ProjectBoardProcessCreate project={project} />
+            </div>
+
             {processes?.map((process, index) =>
                 <div className="aae-project-board__process-wrapper" key={`${process._id}-${index}`}>
                     <ProcessBoardCard process={process} />
---
+++ frontend/src/components/project/update-project/project-board/project-board-process-create/process-create-form/process-create-form.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { createProcess } from "../../../../../../api/project/process.mutations";
+
+export function ProcessCreateForm({ project }) {
+
+    /**
+     * Process creation
+     */
+
+    const [processDataToCreate, setProcessDataToCreate] = useState({
+        title: null,
+        description: null,
+    });
+
+    const [isSaved, setSaved] = useState(false);
+
+    useEffect(() => {
+        if (isSaved) {
+            createProcess({
+                ...processDataToCreate,
+                project: project?._id
+            }).then((createdProcessResponse) => {
+                if (createdProcessResponse?.data?.createProcess) {
+                    setProcessDataToCreate({ title: null, description: null });
+                }
+            });
+            setSaved(false);
+        }
+    }, [isSaved]);
+
+    return <div className="aae-process-create-form">
+        <label>Title</label>
+        <input
+            type="text"
+            value={processDataToCreate.title}
+            onChange={(event) => {
+                setProcessDataToCreate((processData) => ({ ...processData, title: event.target.value }))
+            }} />
+        <label>Description</label>
+        <input
+            type="text"
+            value={processDataToCreate.descrition}
+            onChange={(event) => {
+                setProcessDataToCreate((processData) => ({ ...processData, description: event.target.value }))
+            }} />
+        <button onClick={() => { setSaved(true) }}>Create</button>
+    </div>
+}
