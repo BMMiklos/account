d027a01aa84f6d82ac6e6d24b55215fed19e9a6a
Balázs Miklós <balazs.miklos@yahoo.com>
2022-12-18 21:37:11 +0100

    bejegyzés leírás megjelenítése

--- frontend/src/components/project/update-project/project-board/process-card/entry-board-item/entry-board-item.jsx
+++ frontend/src/components/project/update-project/project-board/process-card/entry-board-item/entry-board-item.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useUpdateProjectDispatch } from "../../../../../../context/update-project.context";
 import "./entry-board-item.css";
 
@@ -5,10 +6,18 @@ export function EntryBoardItem({ entry }) {
 
     const updateProjectDispatch = useUpdateProjectDispatch();
 
+    const [isDescriptionVisible, setDescriptionVisible] = useState(false);
+
     return <div
         draggable
         onDragStart={(event) => { updateProjectDispatch({ type: "SET_ENTRY_MOVE_SETTINGS", payload: { entry } }) }}
         className="aae-entry-board-item">
-        {entry.title}
+
+        <div className="aae-entry-board-item__header">
+            <div className="aae-entry-board-item__title">{entry?.title}</div>
+            <button className="aae-entry-board-item__view-button"
+            onClick={() => { setDescriptionVisible(!isDescriptionVisible) }}></button>
+        </div>
+        {isDescriptionVisible && <div className="aae-entry-board-item__description">{entry?.description}</div>}
     </div>
 }
