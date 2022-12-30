0b2d3f33bc705dac6d2eae3a222a3b622117fa3d
Balázs Miklós <balazs.miklos@yahoo.com>
2022-12-30 19:00:08 +0100

    projekt létrehozás stílusok

--- frontend/src/components/project/create-project.jsx
+++ frontend/src/components/project/create-project.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
 import { createProject } from "../../api/project/project.mutations";
+import "./create-project.css";
+import { useNavigate } from "react-router-dom";
 
 export function CreateProject() {
+
+    const navigate = useNavigate();
 
     const [title, setTitle] = useState();
     const [description, setDescription] = useState();
@@ -10,22 +14,30 @@ export function CreateProject() {
 
     useEffect(() => {
         if (isSaved) {
-            createProject({ title, description })
+            createProject({ title, description }).then((createProjectResponse) => {
+                if (createProjectResponse?.data?.createProject) {
+                    navigate("/projects");
+                }
+            });
         }
         setSaved(false);
-    }, [isSaved])
+    }, [isSaved]);
 
-    return <div>
+    return <div className="create-project">
 
-        <form>
+        <form className="create-project__form">
 
-            <label htmlFor="project-name">Project Name</label>
-            <input type="text" name="project-name" id="project-name" value={title} onChange={(event) => { setTitle(event.target.value) }} />
+            <div className="create-project__form-input-wrapper">
+                <label className="create-project__form-label" htmlFor="project-name">Project Name</label>
+                <input className="create-project__form-input" type="text" name="project-name" id="project-name" value={title} onChange={(event) => { setTitle(event.target.value) }} />
+            </div>
 
-            <label htmlFor="project-description">Project Description</label>
-            <textarea onChange={(event) => { setDescription(event.target.value) }}>{description}</textarea>
+            <div className="create-project__form-input-wrapper">
+                <label className="create-project__form-label" htmlFor="project-description">Project Description</label>
+                <textarea className="create-project__form-textarea" onChange={(event) => { setDescription(event.target.value) }}>{description}</textarea>
+            </div>
 
-            <input type="button" value="Create" onClick={() => { setSaved(true) }} />
+            <input className="create-project__create-button" type="button" value="Create" onClick={() => { setSaved(true) }} />
 
         </form>
 
