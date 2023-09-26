3ae5ac2a91f507722ec07c569e5d3407c2084ed9
Balázs Miklós <balazs.miklos@modim.hu>
2023-09-26 12:25:51 +0200

    Redux hozzáadása

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import { OverlayProvider } from "./context/overlay.context";
 import { router } from "./routes/routes";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <OverlayProvider>
-      <RouterProvider router={router} />
-    </OverlayProvider>
+    <Provider store={store}>
+      <OverlayProvider>
+        <RouterProvider router={router} />
+      </OverlayProvider>
+    </Provider>
   );
 }
 
