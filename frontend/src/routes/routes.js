5132e412428a00eb66dfd24cd9f179cb44b118f8
Balázs Miklós <balazs.miklos@yahoo.com>
2023-10-07 21:00:54 +0200

    komponensek áthelyezése a modulok alá

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Frame } from "../components/layout/frame";
-import { Login } from "../components/login/login";
-import { CreateProject } from "../components/project/create-project";
-import { ProjectTable } from "../components/project/project-table";
-import { UpdateProject } from "../components/project/update-project";
-import { Safe } from "../components/safe/safe";
+import { Frame } from "../modules/layout/frame";
+import { Login } from "../modules/login/login";
+import { CreateProject } from "../modules/project/create-project";
+import { ProjectTable } from "../modules/project/project-table";
+import { UpdateProject } from "../modules/project/update-project";
+import { Safe } from "../modules/safe/safe";
 import { UpdateProjectProvider } from "../context/update-project.context";
-import { Calendar } from "../components/calendar/calendar";
+import { Calendar } from "../modules/calendar/calendar";
 import { CalendarProvider } from "../context/calendar.context";
 import { SafeProvider } from "../context/safe.context";
 
